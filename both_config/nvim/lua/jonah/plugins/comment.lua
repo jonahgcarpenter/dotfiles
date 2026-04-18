50ad52f33a890cbc987a1f12698a045398789cfd
Jonah Carpenter <jonahgcarpenter@gmail.com>
2026-04-18 11:43:28 -0500

    Update comment.lua

--- both_config/nvim/lua/jonah/plugins/comment.lua
+++ both_config/nvim/lua/jonah/plugins/comment.lua
@@ -1,19 +1,27 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
-  config = function()
-    -- import comment plugin safely
-    local comment = require("Comment")
+	"numToStr/Comment.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		-- Skip the backwards compatibility module to prevent treesitter errors
+		vim.g.skip_ts_context_commentstring_module = true
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+		-- Explicitly configure ts_context_commentstring to disable the autocommand
+		require("ts_context_commentstring").setup({
+			enable_autocmd = false,
+		})
 
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx, jsx, svelte, html files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
-  end,
+		-- import comment plugin safely
+		local comment = require("Comment")
+
+		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+		-- enable comment
+		comment.setup({
+			-- for commenting tsx, jsx, svelte, html files
+			pre_hook = ts_context_commentstring.create_pre_hook(),
+		})
+	end,
 }
