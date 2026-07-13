110a37c885c65aa1ea728abe48cc63cbd088dfb5
jonahgcarpenter <jonahgcarpenter@gmail.com>
2026-07-13 17:12:55 -0500

    fixing ts errors, and comment fix

--- both_config/nvim/lua/jonah/plugins/comment.lua
+++ both_config/nvim/lua/jonah/plugins/comment.lua
@@ -5,23 +5,13 @@ return {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
 	config = function()
-		-- Skip the backwards compatibility module to prevent treesitter errors
-		vim.g.skip_ts_context_commentstring_module = true
-
-		-- Explicitly configure ts_context_commentstring to disable the autocommand
-		require("ts_context_commentstring").setup({
-			enable_autocmd = false,
-		})
-
-		-- import comment plugin safely
 		local comment = require("Comment")
+		local ts_pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
 
-		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-		-- enable comment
 		comment.setup({
-			-- for commenting tsx, jsx, svelte, html files
-			pre_hook = ts_context_commentstring.create_pre_hook(),
+			pre_hook = function(ctx)
+				return ts_pre_hook(ctx) or vim.bo.commentstring
+			end,
 		})
 	end,
 }
